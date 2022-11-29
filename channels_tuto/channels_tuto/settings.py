cdbfbc16f273780eacdb5862c41926d4877503ab
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-29 12:00:16 +0100

    chat synchrone channels

--- channels_tuto/channels_tuto/settings.py
+++ channels_tuto/channels_tuto/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = 'channels_tuto.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
